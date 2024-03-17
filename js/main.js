2bf15d338c1030e5738b43d5717dc5256e087e43
Ziyue <Ziyue_xu@outlook.com>
2024-03-17 18:00:13 -0400

    loading screen into main
    when making a page have it, wrap the rest of the page with a div called page--hidden
    
    the loading screen needs to have the class loader

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-
-var document = document 
+var document = document;
 //for css
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
-    item.addEventListener("click", function() {
-        this.classList.toggle("collapsible--expanded");
-    })
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
 );
-
-
-
 
 //document.getElementById("label_fiodjsf").innerText = createAcct;
 
 
+window.addEventListener("load", () => {
+    document.querySelector(".loader").classList.add("loadDone")
+    document.querySelector(".page--hidden").classList.remove("page--hidden")
+})
