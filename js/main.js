276e16db81c1c72a6cb576101d05f7bb791c95c3
Ziyue <Ziyue_xu@outlook.com>
2024-03-17 12:11:21 -0400

    stuff go brrr

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+var document = document 
+//for css
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+    item.addEventListener("click", function() {
+        this.classList.toggle("collapsible--expanded");
+    })
+);
+
+
+
+
+//document.getElementById("label_fiodjsf").innerText = createAcct;
+
+
